a14ad2da432f02a56ebc6523fc6d5636b91d02fc
Kahner Kim <kahner.kim8@gmail.com>
2022-11-08 14:14:25 -0500

    first commit and set up fetch api with nasa apod api

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// Build a nasa apod api using objects, async/await
+// When user clicks on the button call the fetch function
+// The name, picture and description should appear
+
+const apod = {
+    async nasaApi(){
+        let date = document.querySelector('input').value
+        let url = `https://api.nasa.gov/planetary/apod?api_key=qK6wg2vhqJCwaJlBEXZSbWy6JmayigDxp7jDbVaf&date=${date}&thumbs=true`
+        // start the fetch api to get a json object back
+        let getFetch = await fetch(url)
+        let resultFetch = await getFetch.json()
+        if(resultFetch.media_type === 'video'){
+            document.querySelector('h1').innerHTML = resultFetch.title
+            document.querySelector('img').src = resultFetch.thumbnail_url
+            document.querySelector('h3').innerHTML = resultFetch.explanation
+        }else{
+            document.querySelector('h1').innerHTML = resultFetch.title
+            document.querySelector('img').src = resultFetch.hdurl
+            document.querySelector('h3').innerHTML = resultFetch.explanation
+        }
+        console.log(resultFetch);
+    }
+}
+
+document.querySelector('button').addEventListener('click', apod.nasaApi)
