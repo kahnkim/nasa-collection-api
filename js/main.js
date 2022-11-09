45c91a436cdbf576fcf25784f351925df2ff7333
Kahner Kim <kahner.kim8@gmail.com>
2022-11-09 15:29:03 -0500

    Add a try/catch statement to catch any errors that might occur w/ fetch

--- js/main.js
+++ js/main.js
@@ -4,29 +4,33 @@
 
 const apod = {
     async nasaApi(){
-        let date = document.querySelector('input').value
-        let url = `https://api.nasa.gov/planetary/apod?api_key=qK6wg2vhqJCwaJlBEXZSbWy6JmayigDxp7jDbVaf&date=${date}&thumbs=true`
-        // start the fetch api to get a json object back
-        let getFetch = await fetch(url)
-        let resultFetch = await getFetch.json()  
-        if(resultFetch.media_type === 'video'){
-            // If the object returned has a media type of 'video' show iframe and hide img
-            document.querySelector('.nasa__vid').style.display = 'block'
-            document.querySelector('img').style.display = 'none'
+        try{
+            let date = document.querySelector('input').value
+            let url = `https://api.nasa.gov/planetary/apod?api_key=qK6wg2vhqJCwaJlBEXZSbWy6JmayigDxp7jDbVaf&date=${date}&thumbs=true`
+            // start the fetch api to get a json object back
+            let getFetch = await fetch(url)
+            let resultFetch = await getFetch.json()  
+            if(resultFetch.media_type === 'video'){
+                // If the object returned has a media type of 'video' show iframe and hide img
+                document.querySelector('.nasa__vid').style.display = 'block'
+                document.querySelector('img').style.display = 'none'
 
-            document.querySelector('h1').innerHTML = resultFetch.title
-            document.querySelector('iframe').src = resultFetch.url
-            document.querySelector('h2').innerHTML = resultFetch.explanation
-        }else{
-            // Else show the img element and hide video
-            document.querySelector('img').style.display = 'block'
-            document.querySelector('.nasa__vid').style.display = 'none'
+                document.querySelector('h1').innerHTML = resultFetch.title
+                document.querySelector('iframe').src = resultFetch.url
+                document.querySelector('h2').innerHTML = resultFetch.explanation
+            }else{
+                // Else show the img element and hide video
+                document.querySelector('img').style.display = 'block'
+                document.querySelector('.nasa__vid').style.display = 'none'
 
-            document.querySelector('h1').innerHTML = resultFetch.title
-            document.querySelector('img').src = resultFetch.hdurl
-            document.querySelector('h2').innerHTML = resultFetch.explanation
+                document.querySelector('h1').innerHTML = resultFetch.title
+                document.querySelector('img').src = resultFetch.hdurl
+                document.querySelector('h2').innerHTML = resultFetch.explanation
+            }
+            console.log(resultFetch);
+        } catch(err){
+            alert('Error has occurred!')
         }
-        console.log(resultFetch);
     }
 }
 document.querySelector('img').style.display = 'none'
